98eadbd76e7c56ed2982ef5a964c354859f316cf
Manikandan Shanmugam <e0284006@u.nus.edu>
2018-04-04 05:33:32 +0800

    sample qn

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,54 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+   
+
+        public class Person
+        {
+            private string name;
+            public string Name
+            {
+                get { return (name); }
+            }
+            public Person(string n)
+            {
+                name = n;
+            }
+            public virtual void Show()
+            {
+                Console.WriteLine("Person is {0}", Name);
+            }
+        }
+        public class Employee : Person
+        {
+            private string supervisor;
+            public string Supervisor
+            {
+                get { return (supervisor); }
+            }
+            public Employee(string n) : base(n)
+            {
+                supervisor = "Big Boss";
+            }
+            public override void Show()
+            {
+                Console.WriteLine("Employee is {0}", Name);
+            }
+        }
     class Program
     {
         static void Main(string[] args)
         {
+            Person[] employees = {new Person("Ah Kow"),
+                                  new Person("Ah Ter"),
+                                  new Employee("Ah Yeow"),
+                                  new Employee("Ah Beng")};
+            foreach (Person p in employees)
+            {
+                p.Show();
+
+            }
         }
     }
+
 }
